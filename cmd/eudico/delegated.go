3e346c8a96a3471a4976f762d0bacfc65e7c8b7f
Łukasz Magiera <magik6k@gmail.com>
2021-08-02 21:07:53 +0200

    wip genesis

--- cmd/eudico/delegated.go
+++ cmd/eudico/delegated.go
@@ -1,7 +1,28 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/network"
+	bstore "github.com/filecoin-project/lotus/blockstore"
+	"github.com/filecoin-project/lotus/chain/gen"
+	genesis2 "github.com/filecoin-project/lotus/chain/gen/genesis"
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/vm"
+	"github.com/filecoin-project/lotus/cmd/lotus-sim/simulation/mock"
+	"github.com/filecoin-project/lotus/genesis"
+	"github.com/filecoin-project/lotus/journal"
+	"github.com/google/uuid"
+	"github.com/ipfs/go-blockservice"
+	"github.com/ipfs/go-cid"
+	offline "github.com/ipfs/go-ipfs-exchange-offline"
+	"github.com/ipfs/go-merkledag"
+	"github.com/ipld/go-car"
 	"github.com/urfave/cli/v2"
+	"golang.org/x/xerrors"
+	"os"
 
 	"github.com/filecoin-project/lotus/chain/consensus"
 	"github.com/filecoin-project/lotus/chain/consensus/delegcns"
@@ -12,7 +33,79 @@ var delegatedCmd = &cli.Command{
 	Name:  "delegated",
 	Usage: "Delegated consensus testbed",
 	Subcommands: []*cli.Command{
+		delegatedGenesisCmd,
+		delegatedMinerCmd,
 
 		daemonCmd(node.Override(new(consensus.Consensus), delegcns.NewDelegatedConsensus)),
+	},
+}
+
+var delegatedGenesisCmd = &cli.Command{
+	Name:  "genesis",
+	Usage: "Generate genesis for delegated consensus",
+	ArgsUsage: "[miner secpk addr]",
+	Action: func(cctx *cli.Context) error {
+		if cctx.Args().Len() != 2 {
+			return xerrors.Errorf("expected 2 arguments")
+		}
+
+		miner, err := address.NewFromString(cctx.Args().First())
+		if err != nil {
+			return xerrors.Errorf("parsing miner address: %w", err)
+		}
+		if miner.Protocol() != address.SECP256K1 {
+			return xerrors.Errorf("must be secp address")
+		}
+
+		j := journal.NilJournal()
+		bs := bstore.NewMemorySync()
+		syscalls := vm.Syscalls(mock.Verifier)
+		template := genesis.Template{
+			NetworkVersion:   network.Version13,
+			Accounts: []genesis.Actor{{
+				Type:    genesis.TAccount,
+				Balance: types.FromFil(2),
+				Meta:    json.RawMessage(`"` + miner.String() + `""`), // correct??
+			}},
+			Miners:           nil,
+			NetworkName:      "eudico-" + uuid.New().String(),
+			Timestamp:        0,
+		}
+
+		b, err := genesis2.MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)
+		if err != nil {
+			return xerrors.Errorf("make genesis block: %w", err)
+		}
+
+		fmt.Printf("GENESIS MINER ADDRESS: t0%d\n", genesis2.MinerStart)
+
+		f, err := os.OpenFile(cctx.Args().Get(1), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+		if err != nil {
+			return err
+		}
+
+		offl := offline.Exchange(bs)
+		blkserv := blockservice.New(bs, offl)
+		dserv := merkledag.NewDAGService(blkserv)
+
+		if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, f, gen.CarWalkFunc); err != nil {
+			return err
+		}
+
+		log.Warnf("WRITING GENESIS FILE AT %s", f.Name())
+
+		if err := f.Close(); err != nil {
+			return err
+		}
+
+		return nil
+	},
+}
+
+var delegatedMinerCmd = &cli.Command{
+	Name:  "miner",
+	Usage: "run delegated conesensus miner",
+	Action: func(cctx *cli.Context) error {
+		return nil
 	},
 }
