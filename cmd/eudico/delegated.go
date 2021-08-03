16fbe05ccab9e802e9aef2125bc478e89679fcdf
Łukasz Magiera <magik6k@gmail.com>
2021-08-03 14:42:12 +0200

    make delegated consensus genesis generation work

--- cmd/eudico/delegated.go
+++ cmd/eudico/delegated.go
@@ -5,23 +5,31 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/network"
 	bstore "github.com/filecoin-project/lotus/blockstore"
+	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/system"
 	"github.com/filecoin-project/lotus/chain/gen"
 	genesis2 "github.com/filecoin-project/lotus/chain/gen/genesis"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/vm"
+	"github.com/filecoin-project/lotus/chain/wallet"
 	"github.com/filecoin-project/lotus/cmd/lotus-sim/simulation/mock"
 	"github.com/filecoin-project/lotus/genesis"
 	"github.com/filecoin-project/lotus/journal"
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 	"github.com/google/uuid"
 	"github.com/ipfs/go-blockservice"
 	"github.com/ipfs/go-cid"
 	offline "github.com/ipfs/go-ipfs-exchange-offline"
+	cbor "github.com/ipfs/go-ipld-cbor"
 	"github.com/ipfs/go-merkledag"
 	"github.com/ipld/go-car"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
+	"math/rand"
 	"os"
 
 	"github.com/filecoin-project/lotus/chain/consensus"
@@ -43,7 +51,7 @@ var delegatedCmd = &cli.Command{
 var delegatedGenesisCmd = &cli.Command{
 	Name:  "genesis",
 	Usage: "Generate genesis for delegated consensus",
-	ArgsUsage: "[miner secpk addr]",
+	ArgsUsage: "[miner secpk addr] [outfile]",
 	Action: func(cctx *cli.Context) error {
 		if cctx.Args().Len() != 2 {
 			return xerrors.Errorf("expected 2 arguments")
@@ -58,21 +66,47 @@ var delegatedGenesisCmd = &cli.Command{
 		}
 
 		j := journal.NilJournal()
-		bs := bstore.NewMemorySync()
+		bs := bstore.WrapIDStore(bstore.NewMemorySync())
 		syscalls := vm.Syscalls(mock.Verifier)
+
+		memks := wallet.NewMemKeyStore()
+		w, err := wallet.NewWallet(memks)
+		if err != nil {
+			return err
+		}
+
+		vreg, err := w.WalletNew(cctx.Context, types.KTBLS)
+		if err != nil {
+			return err
+		}
+		rem, err := w.WalletNew(cctx.Context, types.KTBLS)
+		if err != nil {
+			return err
+		}
+
 		template := genesis.Template{
-			NetworkVersion:   network.Version13,
+			NetworkVersion: network.Version13,
 			Accounts: []genesis.Actor{{
 				Type:    genesis.TAccount,
 				Balance: types.FromFil(2),
-				Meta:    json.RawMessage(`"` + miner.String() + `""`), // correct??
+				Meta:    json.RawMessage(`{"Owner":"` + miner.String() + `"}`), // correct??
 			}},
 			Miners:           nil,
 			NetworkName:      "eudico-" + uuid.New().String(),
 			Timestamp:        0,
+
+			VerifregRootKey:  genesis.Actor{
+				Type:    genesis.TAccount,
+				Balance: types.FromFil(2),
+				Meta:    json.RawMessage(`{"Owner":"` + vreg.String() + `"}`), // correct??
+			},
+			RemainderAccount: genesis.Actor{
+				Type:    genesis.TAccount,
+				Meta:    json.RawMessage(`{"Owner":"` + rem.String() + `"}`), // correct??
+			},
 		}
 
-		b, err := genesis2.MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)
+		b, err := MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)
 		if err != nil {
 			return xerrors.Errorf("make genesis block: %w", err)
 		}
@@ -89,7 +123,7 @@ var delegatedGenesisCmd = &cli.Command{
 		dserv := merkledag.NewDAGService(blkserv)
 
 		if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, f, gen.CarWalkFunc); err != nil {
-			return err
+			return xerrors.Errorf("write genesis car: %w", err)
 		}
 
 		log.Warnf("WRITING GENESIS FILE AT %s", f.Name())
@@ -108,4 +142,95 @@ var delegatedMinerCmd = &cli.Command{
 	Action: func(cctx *cli.Context) error {
 		return nil
 	},
+}
+
+
+func MakeGenesisBlock(ctx context.Context, j journal.Journal, bs bstore.Blockstore, sys vm.SyscallBuilder, template genesis.Template) (*genesis2.GenesisBootstrap, error) {
+	if j == nil {
+		j = journal.NilJournal()
+	}
+	st, _, err := genesis2.MakeInitialStateTree(ctx, bs, template)
+	if err != nil {
+		return nil, xerrors.Errorf("make initial state tree failed: %w", err)
+	}
+
+	stateroot, err := st.Flush(ctx)
+	if err != nil {
+		return nil, xerrors.Errorf("flush state tree failed: %w", err)
+	}
+
+	// temp chainstore
+	//cs := store.NewChainStore(bs, bs, datastore.NewMapDatastore(), j)
+
+/*	// Verify PreSealed Data
+	stateroot, err = VerifyPreSealedData(ctx, cs, sys, stateroot, template, keyIDs, template.NetworkVersion)
+	if err != nil {
+		return nil, xerrors.Errorf("failed to verify presealed data: %w", err)
+	}
+
+	stateroot, err = SetupStorageMiners(ctx, cs, sys, stateroot, template.Miners, template.NetworkVersion)
+	if err != nil {
+		return nil, xerrors.Errorf("setup miners failed: %w", err)
+	}*/
+
+	store := adt.WrapStore(ctx, cbor.NewCborStore(bs))
+	emptyroot, err := adt0.MakeEmptyArray(store).Root()
+	if err != nil {
+		return nil, xerrors.Errorf("amt build failed: %w", err)
+	}
+
+	mm := &types.MsgMeta{
+		BlsMessages:   emptyroot,
+		SecpkMessages: emptyroot,
+	}
+	mmb, err := mm.ToStorageBlock()
+	if err != nil {
+		return nil, xerrors.Errorf("serializing msgmeta failed: %w", err)
+	}
+	if err := bs.Put(mmb); err != nil {
+		return nil, xerrors.Errorf("putting msgmeta block to blockstore: %w", err)
+	}
+
+	log.Infof("Empty Genesis root: %s", emptyroot)
+
+	tickBuf := make([]byte, 32)
+	_, _ = rand.Read(tickBuf)
+	genesisticket := &types.Ticket{
+		VRFProof: tickBuf,
+	}
+
+	b := &types.BlockHeader{
+		Miner:                 system.Address,
+		Ticket:                genesisticket,
+		Parents:               []cid.Cid{},
+		Height:                0,
+		ParentWeight:          types.NewInt(0),
+		ParentStateRoot:       stateroot,
+		Messages:              mmb.Cid(),
+		ParentMessageReceipts: emptyroot,
+		BLSAggregate:          nil,
+		BlockSig:              nil,
+		Timestamp:             template.Timestamp,
+		ElectionProof:         new(types.ElectionProof),
+		BeaconEntries: []types.BeaconEntry{
+			{
+				Round: 0,
+				Data:  make([]byte, 32),
+			},
+		},
+		ParentBaseFee: abi.NewTokenAmount(build.InitialBaseFee),
+	}
+
+	sb, err := b.ToStorageBlock()
+	if err != nil {
+		return nil, xerrors.Errorf("serializing block header failed: %w", err)
+	}
+
+	if err := bs.Put(sb); err != nil {
+		return nil, xerrors.Errorf("putting header to blockstore: %w", err)
+	}
+
+	return &genesis2.GenesisBootstrap{
+		Genesis: b,
+	}, nil
 }
