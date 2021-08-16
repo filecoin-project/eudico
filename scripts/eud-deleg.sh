7d783c552daf5b2567974cefde88f569e4b748b5
Łukasz Magiera <magik6k@gmail.com>
2021-08-16 19:54:09 +0200

    script for starting delegated devnets

---
+++ scripts/eud-deleg.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+make eudico
+rm -rvf ~/.eudico
+./eudico delegated genesis $(echo f1* | tr '.' ' ' | awk '{print $1}') gen.gen
+
+tmux \
+    new-session  './eudico delegated daemon --genesis=gen.gen' \; \
+    split-window './eudico wait-api; ./eudico wallet import --format=hex-lotus 'f1*'; ./eudico delegated miner; sleep 2'
