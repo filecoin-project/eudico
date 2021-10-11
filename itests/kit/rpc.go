e83abd4dc0898f3d2d730edf6beeeed51d6efbba
Alfonso de la Rocha <adlrocha@tutamail.com>
2021-10-11 09:35:56 +0200

    fix itests with new API signature

--- itests/kit/rpc.go
+++ itests/kit/rpc.go
@@ -32,7 +32,7 @@ func CreateRPCServer(t *testing.T, handler http.Handler, listener net.Listener) 
 }
 
 func fullRpc(t *testing.T, f *TestFullNode) *TestFullNode {
-	handler, err := node.FullNodeHandler(f.FullNode, false)
+	handler, err := node.FullNodeHandler("", f.FullNode, false)
 	require.NoError(t, err)
 
 	l, err := net.Listen("tcp", "127.0.0.1:0")
