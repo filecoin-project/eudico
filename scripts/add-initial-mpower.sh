c881cdfd33d4e07227d67e52ccbf9b004a2e8949
Sarah Azouvi <sarah8@bitbucket.org>
2022-02-22 17:15:25 +0000

    debug

--- scripts/add-initial-mpower.sh
+++ scripts/add-initial-mpower.sh
@@ -1,5 +1,2 @@
 EUDICO_PATH=$PWD/data/alice ./eudico send --from t1d2xrzcslx7xlbbylc5c3d5lvandqw4iwl6epxba --method 2 --params-json "{\"Miners\":[\"12D3KooWMBbLLKTM9Voo89TXLd98w4MjkJUych6QvECptousGtR4\", \"12D3KooWNTyoBdMB9bpSkf7PVWR863ejGVPq9ssaaAipNvhPeQ4t\", \"12D3KooWF1aFCGUtsGEaqNks3QADLUDZxW7ot7jZPSoDiAFKJuM6\"]}" t065 0
 
-
-EUDICO_PATH=$PWD/data/alice ./eudico send --from t1d2xrzcslx7xlbbylc5c3d5lvandqw4iwl6epxba --method 4 --params-json "{\"PubKey\":[\"1\", \"0\", \"1\"]}" t065 0
-EUDICO_PATH=$PWD/data/alice ./eudico send --from t1d2xrzcslx7xlbbylc5c3d5lvandqw4iwl6epxba --method 4 --params-json "[0, 0, 1]" t065 0
