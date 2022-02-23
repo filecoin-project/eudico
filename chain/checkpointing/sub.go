4a125343beebb092c20461a7188d0098ada3ceca
Sarah Azouvi <sarah8@bitbucket.org>
2022-02-23 10:54:19 +0000

    debug

--- chain/checkpointing/sub.go
+++ chain/checkpointing/sub.go
@@ -300,6 +300,7 @@ func (c *CheckpointingSub) listenCheckpointEvents(ctx context.Context) {
 		fmt.Println("start syncing")
 
 		if !c.synced {
+			fmt.Println("not synced")
 			// Are we synced ?
 			// Replace this WaitForSync logic with this function
 			// https://github.com/Zondax/eudico/blob/1de9d0f773e49b61cd405add93c3c28c9f74cb38/node/modules/services.go#L104
