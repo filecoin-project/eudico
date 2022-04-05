ea561ff683fca2c51a919b91622dd7707e13301c
Sarah Azouvi <sarah8@bitbucket.org>
2022-04-05 18:15:50 +0100

    more logs to debug k8s issues

--- chain/checkpointing/sub.go
+++ chain/checkpointing/sub.go
@@ -443,7 +443,7 @@ func (c *CheckpointingSub) matchNewConfig(ctx context.Context, oldTs, newTs *typ
 	/*
 		Now we compared old Power Actor State and new Power Actor State
 	*/
-	log.Infow("New config detected")
+	
 
 	// If no changes in configuration
 	if sameStringSlice(oldSt.Miners, newSt.Miners) {
@@ -451,6 +451,7 @@ func (c *CheckpointingSub) matchNewConfig(ctx context.Context, oldTs, newTs *typ
 	}
 	// only the participants in the new config need to trigger the DKG
 	for _, participant := range newSt.Miners {
+		log.Infow("New config detected")
 		if participant == c.host.ID().String() {
 			diff.newMiners = newSt.Miners
 			c.newParticipants = newSt.Miners
@@ -463,8 +464,9 @@ func (c *CheckpointingSub) matchNewConfig(ctx context.Context, oldTs, newTs *typ
 
 func (c *CheckpointingSub) matchCheckpoint(ctx context.Context, oldTs, newTs *types.TipSet, oldSt, newSt mpower.State, diff *diffInfo) (bool, error) {
 	// we are checking that the list of mocked actor is not empty before starting the checkpoint
-	log.Infow("New checkpoint to start")
+	
 	if newTs.Height()%checkpointFrequency == 0 && len(oldSt.Miners) > 0 && (c.taprootConfig != nil || c.newTaprootConfig != nil) && len(newSt.PublicKey)>0 {
+		log.Infow("New checkpoint to start")
 		cp := oldTs.Key().Bytes() // this is the checkpoint
 		diff.cp = cp
 
