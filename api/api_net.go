0369bd5477260bc8e3b8845c49b8b534aa64415f
Denis Kolegov <dnkolegov@gmail.com>
2022-07-21 11:58:59 +0300

    Update api/api_net.go
    Co-authored-by: adlrocha <adlrocha@tutamail.com>

--- api/api_net.go
+++ api/api_net.go
@@ -65,7 +65,7 @@ type Net interface {
 	// ID returns peerID of libp2p node backing this API
 	ID(context.Context) (peer.ID, error) //perm:read
 	// PrivKey returns libp2p host's private key.
-	// This is temporal experimental function that must be removed in the future.
+	// FIXME: This is temporal experimental function that must be removed in the future.
 	PrivKey(context.Context) ([]byte, error) //perm:read
 }
 
