9ef7230538dbf02736b548c243d6ea2d91b35342
cryptoAtWill <>
2022-07-29 16:05:56 +0000

    revert go address

--- chain/consensus/hierarchical/subnet/submgr/manager.go
+++ chain/consensus/hierarchical/subnet/submgr/manager.go
@@ -21,6 +21,7 @@ import (
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/v1api"
